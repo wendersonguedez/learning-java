86970ad0767ba51c142972c13bce0678e9e2433a
wenderson <wendersonguedes6@gmail.com>
2025-03-31 23:32:01 -0300

    adicionado exemplos de tipos primitivos

--- src/tiposPrimitivos/TiposPrimitivos.java
+++ src/tiposPrimitivos/TiposPrimitivos.java
@@ -2,9 +2,14 @@ package tiposPrimitivos;
 
 public class TiposPrimitivos {
      public static void main(String[] args) {
+         String nome = "wenderson";
          int idade = 25;
+         double salarioMensal = 4500.00;
+         char genero = 'M';
+         boolean taEmpregado = true;
+         String resultado = (taEmpregado) ? "tem emprego" : "ta desempregado";
 
-         System.out.println("Idade: " + idade);
+         System.out.println(nome + " " + resultado + ", recebe R$" + salarioMensal + " e tem " + idade + " de idade");
      }
 
      public void example() {
