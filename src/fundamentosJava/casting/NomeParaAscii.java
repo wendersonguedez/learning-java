3cdb11a461cd65ff6c9da86a771882e802b70fe4
wenderson <wendersonguedes6@gmail.com>
2025-04-01 22:58:07 -0300

    feat: exemplo de casting com tabela ascii

---
+++ src/fundamentosJava/casting/NomeParaAscii.java
@@ -0,0 +1,21 @@
+package fundamentosJava.casting;
+
+import java.util.Scanner;
+
+public class NomeParaAscii {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("O nome " + nome + " na tabela ASCII ficaria: ");
+        for (int i = 0; i < nome.length(); i++) {
+            char caracterNome = nome.charAt(i);
+            int ascii = (int) caracterNome;
+            System.out.println(caracterNome + " → " + ascii);
+        }
+
+        scanner.close();
+    }
+}
